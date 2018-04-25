c043d9cae6d8632450e931b76aa63c9f890020e8
imooc <1473357859@qq.com>
2018-04-25 11:06:54 +0800

    Initial commit

---
+++ springboottomcatjetty/src/main/java/com/house/sell/SpringboottomcatjettyApplication.java
@@ -0,0 +1,12 @@
+package com.house.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottomcatjettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottomcatjettyApplication.class, args);
+	}
+}
